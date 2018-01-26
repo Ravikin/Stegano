7c40d30803dfe2adef2f77c41dc81ec3a44fb67b
Ravikin <ravi@uwcontrol.pl>
2018-01-26 01:52:14 +0100

    Stegano first steps
    Final upload of asci array training thing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Stegano
+My entrance to steganography world, mainly using python3 (3 for god's sake!).
+
+Take a look around ;)
+
---
+++ array.py
@@ -0,0 +1,42 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#/		Authored by TheRavikin (https://github.com/Ravikin)    \#
+#\	   ravi@uwcontrol.pl   |   https://steemit.com/@theravikin /#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# This lil think is printing given image in asci style array with grayscale values
+# Some tips for me for later 
+# PIL convert options
+# https://pillow.readthedocs.io/en/latest/handbook/concepts.html#modes
+from numpy import *
+from PIL import Image
+from PIL import ImageOps
+import sys
+import os
+import time
+
+# Checking for numpy loaded properly
+modulename = 'numpy'
+	if modulename not in sys.modules:
+    print("You have not imported the {} module").format(modulename)
+
+# Loading local image for ~fun~
+img = Image.open('img.png')
+width, height = img.size
+
+# Converting to grayscale
+im = ImageOps.grayscale(img)
+
+# Saving not needed in this case
+# im.save(os.getcwd() + '\\Snap__' + str(int(time.time())) + '.png', 'PNG')
+
+a = []
+ for x in range(0,width):
+    for y in range(0,height):
+		a.append(im.getpixel((x,y)))
+
+a = array(a)
+final = a.reshape(width,height)
+
+print(final)
+
+# cya! well done!
+
